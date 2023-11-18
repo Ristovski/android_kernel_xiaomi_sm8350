a40f1f60d6d4d9085bccda210f6375a55e09ff61
Rafael Ristovski <rafael.ristovski@gmail.com>
2023-11-19 00:06:00 +0100

    staging: qca-wifi-host-cmn: Demote verbosity to info

--- drivers/staging/qca-wifi-host-cmn/htc/htc_recv.c
+++ drivers/staging/qca-wifi-host-cmn/htc/htc_recv.c
@@ -440,7 +440,7 @@ QDF_STATUS htc_rx_completion_handler(void *Context, qdf_nbuf_t netbuf,
 				break;
 #ifdef HTC_MSG_WAKEUP_FROM_SUSPEND_ID
 			case HTC_MSG_WAKEUP_FROM_SUSPEND_ID:
-				AR_DEBUG_PRINTF(ATH_DEBUG_ANY,
+				AR_DEBUG_PRINTF(ATH_DEBUG_INFO,
 					("Received initial wake up"));
 				htc_credit_record(HTC_INITIAL_WAKE_UP,
 					pEndpoint->TxCredits,
@@ -451,7 +451,7 @@ QDF_STATUS htc_rx_completion_handler(void *Context, qdf_nbuf_t netbuf,
 					info->target_initial_wakeup_cb(
 						info->target_psoc);
 				else
-					AR_DEBUG_PRINTF(ATH_DEBUG_ANY,
+					AR_DEBUG_PRINTF(ATH_DEBUG_INFO,
 						("No initial wake up cb"));
 				break;
 #endif
